44585f342fdbcc95565fbff498e6fcd622ef4180
Jarod <jarodrlima@gmail.com>
2022-08-16 15:47:27 -0300

    Initial Commit

--- Exercícios simples FATEC.py
+++ Exercícios simples FATEC.py
@@ -5,14 +5,17 @@ soma = 0
 if num > 0:
     for i in range(num+1):
         soma = soma + i
-    print(f" A soma é {soma} ")
+        print(f" Para numero: {i} a soma é: {soma} ")
+    print(f'A soma dos números {num} é {soma}')
+elif num < 0:
+    print('A soma é para números positivos')
+
 --------------------------------------------------------------------------------
-# Somo os algoritmos num = '10' Ex(1+0) 10 = 1
+# Soma os algoritmos num = '10' Ex(1+0) 10 = 1
 soma = 0
 b = str(num)  # Transformo em str para poder usar posição. Ex: "b[1] = 2"
 for i in range(len(b)):
     soma += int(b[i])
 print('O resultado da soma é {}'.format(soma))
--------------------------------------------------------------------------------------
-
+---------------------------------------------------------------------------------
 """
