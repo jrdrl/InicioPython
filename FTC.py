d96011ed7e1795d6b3a0684bca333258172e0aac
Jarod <jarodrlima@gmail.com>
2022-09-15 10:47:06 -0300

    Initial Commit

--- FTC.py
+++ FTC.py
@@ -8,3 +8,7 @@ for c in range(1, num + 1):
         print('\033[31m', end='')
     print(f'{c}', end='')
 print(f'O número {num} foi divisível {tot} vezes')
+if tot == 2:
+    print(f'{num}É primo')
+else:
+    print(f'{num}Não é primo')
